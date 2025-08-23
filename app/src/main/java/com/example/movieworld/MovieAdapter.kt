4778687afbd1ff99e11b3f7c0212ce7d7603807e
amberting_ <amberting2004@gmail.com>
2025-08-23 14:56:37 +0800

    Initial setup
    Added files and gradle dependencies

---
+++ app/src/main/java/com/example/movieworld/MovieAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.movieworld
+
+class MovieAdapter {
+}
---
+++ app/src/main/java/com/example/movieworld/MovieListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.movieworld
+
+import androidx.lifecycle.ViewModel
+
+class MovieListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
